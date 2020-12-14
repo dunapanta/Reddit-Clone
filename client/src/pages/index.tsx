d69dc35925f50c03301a8c6ca4abce69cab41220
Daniel Unapanta <dunapanta2@gmail.com>
2020-12-14 17:13:41 -0600

    Agregando logo y search bar

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+import RedditLogo from '../images/reddit.svg'
 
 export default function Home() {
   return (
@@ -7,7 +10,28 @@ export default function Home() {
       <Head>
         <title>Reddit Clone</title>
       </Head>
-        <div className="fixed top-0 z-10 flex items-center justify-center h-12 insert-x-0"></div>
+        <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 bg-white">
+          {/* Logo and title */}
+          <div className="flex items-center">
+            <Link href="/">
+              <a>
+                <RedditLogo className="w-8 h-8 mr-2" />
+              </a>
+            </Link>
+            <span className="text-2xl font-semibold">
+              <Link href="/">Reddit Clone</Link>
+            </span>
+          </div>
+          {/* Search Input */}
+          <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+            <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
+            <input 
+              type="text"
+              className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+            />
+          </div>
+          {/* Auth Buttons */}
+        </div>
     </div>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
       },
       spacing:{
         70: '17.5rem',
+        160: '40rem',
       }
     },
   },
