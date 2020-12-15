79cbe923ba3343c5401d88e0de3aede7a32bf2be
Daniel Unapanta <dunapanta2@gmail.com>
2020-12-15 10:35:04 -0600

    Extrayendo navbar en su propio componente

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,27 @@
 import { AppProps } from 'next/app'
 import Axios from 'axios'
+import { Fragment } from 'react'
+import { useRouter } from 'next/router'
 
 import '../styles/globals.css'
+
+import Navbar from '../components/Navbar'
 
 Axios.defaults.baseURL = 'http://localhost:5000/api'
 Axios.defaults.withCredentials = true
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter()
+  /* const authRoute = pathname === '/register' || pathname == '/login' */
+  const authRoutes = ['/register', '/login']
+  const authRoute = authRoutes.includes(pathname)
+
+  return (
+    <Fragment>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
 
 export default MyApp
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
-
-import RedditLogo from '../images/reddit.svg'
 
 export default function Home() {
   return (
@@ -9,41 +6,6 @@ export default function Home() {
       <Head>
         <title>Reddit Clone</title>
       </Head>
-        <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
-          {/* Logo and title */}
-          <div className="flex items-center">
-            <Link href="/">
-              <a>
-                <RedditLogo className="w-8 h-8 mr-2" />
-              </a>
-            </Link>
-            <span className="text-2xl font-semibold">
-              <Link href="/">Reddit Clone</Link>
-            </span>
-          </div>
-          {/* Search Input */}
-          <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
-            <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
-            <input 
-              type="text"
-              className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
-              placeholder="Buscar"
-            />
-          </div>
-          {/* Auth Buttons */}
-          <div className="flex">
-            <Link href="/login">
-              <a className="w-32 py-1 mr-4 leading-5 hollow blue button">
-                Iniciar Sesión
-              </a>
-            </Link>
-            <Link href="/register">
-              <a className="w-32 py-1 leading-5 blue button">
-                Registrarse
-              </a>
-            </Link>
-          </div>
-        </div>
     </div>
   )
 }
