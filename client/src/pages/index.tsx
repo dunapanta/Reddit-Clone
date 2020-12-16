257ad3a1835065477ff5e5fe9a72b6f914272fb1
Daniel Unapanta <dunapanta2@gmail.com>
2020-12-16 16:03:14 -0600

    Agregando fecha con dayjs

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,15 @@
 import Axios from 'axios'
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import dayjs from 'dayjs'
+import relativeTime from  'dayjs/plugin/relativeTime'
+import 'dayjs/locale/es'
 
 import { Post } from '../types'
+
+// para el tiempo en ves de momentjs
+dayjs.extend(relativeTime)
 
 export default function Home() {
 
@@ -30,7 +37,33 @@ export default function Home() {
               </div>
               {/* Post data section */}
               <div className="w-full p-2">
-                <p>{post.body}</p>
+                <div className="flex items-center">
+                  <Link href={`/r/${post.subName}`}>
+                    <img 
+                      src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y" 
+                      className="w-6 h-6 mr-1 rounded-full cursor-pointer"
+                    />
+                  </Link>
+                  <Link href={`/r/${post.subName}`}>
+                    <a className="text-xs font-bold hover:underline">
+                      /r/{post.subName}
+                    </a>
+                  </Link>
+                  <p className="text-xs text-gray-500">
+                    <span className="mx-1">•</span> 
+                    Posteado por
+                    <Link href={`/u/user`}>
+                      <a className="mx-1 hover:underline">
+                        /u/user
+                      </a>
+                    </Link>
+                    <Link href={`/r/${post.subName}/${post.identifier}/${post.slug}`}>
+                      <a className="mx-1 hover:underline">
+                        {dayjs(post.createdAt).locale("es").fromNow()}
+                      </a>
+                    </Link>
+                  </p>
+                </div>
               </div>
             </div>
           ))}
