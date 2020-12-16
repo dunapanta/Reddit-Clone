2595cb2aa7dbed98139dcbbce43213d2cae8e930
Daniel Unapanta <dunapanta2@gmail.com>
2020-12-16 13:07:57 -0600

    Cambio screen size en plugins

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,10 +2,13 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div className="pt-12">
       <Head>
         <title>Reddit Clone</title>
       </Head>
+      <div className="container">
+        <h1>Post Recientes</h1>
+      </div>
     </div>
   )
 }
