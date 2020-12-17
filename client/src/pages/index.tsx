86245e57412ed6bef16833f7434186c52db2a9e0
Daniel Unapanta <dunapanta2@gmail.com>
2020-12-16 22:04:53 -0600

    Implementando Server Side Rendering

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,19 +7,20 @@ import relativeTime from  'dayjs/plugin/relativeTime'
 import 'dayjs/locale/es'
 
 import { Post } from '../types'
+import { GetServerSideProps } from 'next'
 
 // para el tiempo en ves de momentjs
 dayjs.extend(relativeTime)
 
-export default function Home() {
+export default function Home({ posts }) {
 
-  const [posts, setPosts] = useState<Post[]>([])
+  /* const [posts, setPosts] = useState<Post[]>([])
 
   useEffect( () => {
     Axios.get('/posts')
     .then(res => setPosts(res.data))
     .catch(err => console.log(err))
-  },[]) 
+  },[])  */
 
   return (
     <div className="pt-12">
@@ -100,4 +101,18 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+
+//Function for Server Side Rendering
+// esta funcion se ejecuta en el lado del servidor antes que esta pagina se cree y renderice
+// permite renderizar la pagina ya con los datos
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try{
+    const res = await Axios.get('/posts')
+
+    return { props: { posts: res.data }}
+  }catch(err){
+    return { props: { error: "Algo ha salido mal"}}
+  }
 }
