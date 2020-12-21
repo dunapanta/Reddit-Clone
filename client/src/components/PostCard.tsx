6767d83e8a7cd4a1f5d8ba43c4622bee347135e5
Daniel Unapanta <dunapanta2@gmail.com>
2020-12-21 10:12:00 -0600

    Refactor PostCard en componente

---
+++ client/src/components/PostCard.tsx
@@ -0,0 +1,82 @@
+import Link from 'next/link'
+import dayjs from 'dayjs'
+import relativeTime from  'dayjs/plugin/relativeTime'
+import 'dayjs/locale/es'
+
+
+import { Post } from '../types'
+
+// para el tiempo en ves de momentjs
+dayjs.extend(relativeTime)
+
+interface PostCardProps {
+    post:Post
+}
+
+export default function PostCard({ post }) {
+    return (
+        <div key={post.identifier} className="flex mb-4 bg-white rounded">
+              {/* Vote section */}
+              <div className="w-10 text-center bg-gray-200 rounded-l">
+                <p>V</p>
+              </div>
+              {/* Post data section */}
+              <div className="w-full p-2">
+                <div className="flex items-center">
+                  <Link href={`/r/${post.subName}`}>
+                    <img 
+                      src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y" 
+                      className="w-6 h-6 mr-1 rounded-full cursor-pointer"
+                    />
+                  </Link>
+                  <Link href={`/r/${post.subName}`}>
+                    <a className="text-xs font-bold hover:underline">
+                      /r/{post.subName}
+                    </a>
+                  </Link>
+                  <p className="text-xs text-gray-500">
+                    <span className="mx-1">•</span> 
+                    Posteado por
+                    <Link href={`/u/${post.username}`}>
+                      <a className="mx-1 hover:underline">
+                        /u/{post.username}
+                      </a>
+                    </Link>
+                   {/*  <Link href={`/r/${post.subName}/${post.identifier}/${post.slug}`}> */}
+                   <Link href={post.url}>
+                      <a className="mx-1 hover:underline">
+                        {dayjs(post.createdAt).locale("es").fromNow()}
+                      </a>
+                    </Link>
+                  </p>
+                </div>
+                <Link href={post.url}>
+                  <a className="my-1 text-lg font-medium">
+                    {post.title}
+                  </a>
+                </Link>
+                {post.body && <p className="my-1 text-sm">{post.body}</p>}
+                
+                {/* Buttons */}
+                <div className="flex">
+                  <Link href={post.url}>
+                    <a >
+                      <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+                        <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                        <span className="font-bold">Comentarios</span>
+                      </div>
+                    </a>
+                  </Link>
+                    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+                      <i className="mr-1 fas fa-share fa-xs"></i>
+                      <span className="font-bold">Compartir</span>
+                    </div>
+                    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+                      <i className="mr-1 fas fa-bookmark fa-xs"></i>
+                      <span className="font-bold">Guardar</span>
+                    </div>
+                </div>
+              </div>
+            </div>
+    )
+}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,6 +7,7 @@ import relativeTime from  'dayjs/plugin/relativeTime'
 import 'dayjs/locale/es'
 
 import { Post } from '../types'
+import PostCard from '../components/PostCard'
 /* import { GetServerSideProps } from 'next' */
 
 // para el tiempo en ves de momentjs
@@ -31,69 +32,7 @@ export default function Home() {
         {/* Posts feed */}
         <div className="w-160">
           {posts.map(post =>(
-            <div key={post.identifier} className="flex mb-4 bg-white rounded">
-              {/* Vote section */}
-              <div className="w-10 text-center bg-gray-200 rounded-l">
-                <p>V</p>
-              </div>
-              {/* Post data section */}
-              <div className="w-full p-2">
-                <div className="flex items-center">
-                  <Link href={`/r/${post.subName}`}>
-                    <img 
-                      src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y" 
-                      className="w-6 h-6 mr-1 rounded-full cursor-pointer"
-                    />
-                  </Link>
-                  <Link href={`/r/${post.subName}`}>
-                    <a className="text-xs font-bold hover:underline">
-                      /r/{post.subName}
-                    </a>
-                  </Link>
-                  <p className="text-xs text-gray-500">
-                    <span className="mx-1">•</span> 
-                    Posteado por
-                    <Link href={`/u/${post.username}`}>
-                      <a className="mx-1 hover:underline">
-                        /u/{post.username}
-                      </a>
-                    </Link>
-                   {/*  <Link href={`/r/${post.subName}/${post.identifier}/${post.slug}`}> */}
-                   <Link href={post.url}>
-                      <a className="mx-1 hover:underline">
-                        {dayjs(post.createdAt).locale("es").fromNow()}
-                      </a>
-                    </Link>
-                  </p>
-                </div>
-                <Link href={post.url}>
-                  <a className="my-1 text-lg font-medium">
-                    {post.title}
-                  </a>
-                </Link>
-                {post.body && <p className="my-1 text-sm">{post.body}</p>}
-                
-                {/* Buttons */}
-                <div className="flex">
-                  <Link href={post.url}>
-                    <a >
-                      <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                        <i className="mr-1 fas fa-comment-alt fa-xs"></i>
-                        <span className="font-bold">Comentarios</span>
-                      </div>
-                    </a>
-                  </Link>
-                    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                      <i className="mr-1 fas fa-share fa-xs"></i>
-                      <span className="font-bold">Compartir</span>
-                    </div>
-                    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                      <i className="mr-1 fas fa-bookmark fa-xs"></i>
-                      <span className="font-bold">Guardar</span>
-                    </div>
-                </div>
-              </div>
-            </div>
+            <PostCard key={post.identifier} post={post} />
           ))}
 
         </div>
