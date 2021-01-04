05a93b9064e2d6aa8c669f398d89f9762c0e1321
Daniel Unapanta <dunapanta2@gmail.com>
2021-01-04 17:42:26 -0600

    Agregando imagen de banner y usuario

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,15 +1,20 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 import useSWR from 'swr'
+import Image from 'next/image'
+
 import PostCard from "../../components/PostCard";
+import { Sub } from "../../types";
 
 
-export default function Sub(){
+export default function SubPage(){
     const router = useRouter()
 
     const subName = router.query.sub
 
     //en SWR si se pasa null no hace ninguna llamada
-    const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : null)
+    const { data: sub, error } = useSWR<Sub>(subName ? `/subs/${subName}` : null)
 
     if(error) router.push('/')
 
@@ -26,12 +31,51 @@ export default function Sub(){
     }
 
     return(
-        <div className="container flex pt-5">
-        { sub && (
-            <div className="w-160">
-                {postsMarkup}
-            </div>
-        )}
+        <div>
+            <Head>
+                <title>
+                    {sub?.title}
+                </title>
+            </Head>
+            { sub && (
+                <Fragment>
+                    {/* Sub info and images */}
+                    <div>
+                        {/* Banner Image */}
+                        <div className="bg-blue-500">
+                            {sub.bannerUrl ? (
+                                <div className="h-56 bg-blue-500" style={{
+                                    backgroundImage: `url(${sub.bannerUrl})`,
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center'
+
+                                }}></div>
+                            ): (
+                                <div className="h-20 bg-blue-500"></div>
+                            )}
+                        </div>
+                        {/* Sub meta data */}
+                        <div className="h-20 bg-white">
+                            <div className="container flex">
+                                <Image
+                                    src={sub.imageUrl}
+                                    alt="Sub"
+                                    className="rounded-full"
+                                    width={80}
+                                    height={80}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    {/* Posts & Sidebar */}
+                    <div className="container flex pt-5">
+                        <div className="w-160">
+                            {postsMarkup}
+                        </div>
+                    </div>
+                </Fragment>
+            )}
         </div>
     )
         
