76f689d6b79d3b01580cdb9356fce29a9146e520
Daniel Unapanta <dunapanta2@gmail.com>
2021-01-28 18:08:29 -0600

    Comment secure

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -84,7 +84,7 @@ const login = async (req: Request, res: Response) => {
         // path where the cookie is valid
         res.set('Set-Cookie', cookie.serialize('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            // secure: process.env.NODE_ENV === 'production',
             sameSite: 'strict',
             maxAge: 3600,
             path: '/',
@@ -108,7 +108,7 @@ const logout = (_: Request, res: Response) => {
 
     res.set('Set-Cookie', cookie.serialize('token', '', {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',
         expires: new Date(0),
         path: '/',
